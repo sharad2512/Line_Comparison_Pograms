4fd97d4244338d0ec38ac0d5b56974e163b0ac98
sharad2512 <shnarute95@gmail.com>
2022-12-28 14:57:16 +0530

    ADDED : Code to find Length of Line

--- src/main/java/com/bridgelabzLineComparison/LineComparison.java
+++ src/main/java/com/bridgelabzLineComparison/LineComparison.java
@@ -1,7 +1,22 @@
 package com.bridgelabzLineComparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Program");
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            float x1, y1, x2, y2, length1;
+
+            System.out.println(" Enter point for x1 and y1");
+            x1 = scan.nextFloat();
+            y1 = scan.nextFloat();
+
+            System.out.println(" Enter points for x2 and y2 ");
+            x2 = scan.nextFloat();
+            y2 = scan.nextFloat();
+
+            length1 = (float) Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+            System.out.println("Length of line one is " + length1);
+        }
+
     }
-}
